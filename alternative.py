995d10f4f9e5f697a1f9c93f0115242706822056
Kyle Marunda <97623896+Elyk-Adnuram@users.noreply.github.com>
2024-02-23 14:16:01 +0200

    Add files via upload

---
+++ alternative.py
@@ -0,0 +1,27 @@
+'''Program to make amendments to alternate characters'''
+
+# example_string = "Hello World"
+# output_string = ""
+# counter = 0 
+
+# #Loop through string and create condition to target every 2nd character
+# for letter in example_string: 
+#     if counter % 2 == 0:
+#         output_string += letter.upper()
+#     else:
+#         output_string += letter.lower()
+#     counter += 1
+# print(output_string)
+
+output_string2 = []
+example_string2 = "I am learning to code"
+count = 0
+#split the string into a list
+split_string = example_string2.split()
+for word in split_string:
+    if count % 2 == 0:
+        output_string2 += 
+    else:
+        output_string2.append(str((word.upper())))
+count += 1
+print(output_string2)
